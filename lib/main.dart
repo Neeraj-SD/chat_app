4f48f2af169cf2cf25f0c0a6af607303b06db4f1
Neeraj S D <neerajsd04@gmail.com>
2022-08-15 19:14:12 +0530

    commit before firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
+import 'dart:developer';
+
 import 'package:chat_app/Auth/controllers/auth_controller.dart';
 import 'package:chat_app/Auth/views/auth_screen.dart';
 import 'package:chat_app/Chat/views/chat_overview_screen.dart';
 import 'package:chat_app/controller_injector.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> onBackgroundMessageHandler(RemoteMessage message) async {
+  log('fcm Message data:${message.data}');
+  log('fcm Message body${message.notification?.body}');
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getToken();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(onBackgroundMessageHandler);
+  // di.sl<Fcm>();
+
+  FirebaseMessaging.onMessage.listen((event) {
+    log('On message triggered');
+    log(event.notification?.title ?? '');
+    log(event.notification?.body ?? '');
+  });
   ControllerInjector().init();
+
   runApp(MyApp());
 }
 
